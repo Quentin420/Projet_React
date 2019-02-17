d42ad9dea1441eefdb54975770b8fb0a1e5eaedb
Quentin Lemerle <quentin.lemerle@edu.ece.fr>
2019-02-17 18:35:31 +0100

    Correction problème fichiers sources + mise en place reactstrap

---
+++ projet/src/Container/Admin.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {Container, Row, Col} from 'reactstrap';
+
+
+class Admin extends Component {
+  render() {
+    return (
+      <div >
+      <p>Coucou</p>
+        
+      </div>
+    );
+  }
+}
+
+export default Admin;
---
+++ projet/src/Container/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {Container, Row, Col} from 'reactstrap';
+
+
+class Home extends Component {
+  render() {
+    return (
+    	<Container>
+      <div >
+         <p>Salut</p>
+      </div>
+      </Container>
+    );
+  }
+}
+
+export default Home;
